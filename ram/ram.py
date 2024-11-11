68abcaeb23c0ef98c56e589bbb7a661b2a8f02f6
Your Name <youremail@domain.com>
2024-11-11 12:47:13 -0600

    edited the code

--- ram/ram.py
+++ ram/ram.py
@@ -1,6 +1,5 @@
 print ("my name is binod gaihre")
 print ('I am studing data science and visualization in northwestern university')
-i = 4
-j= 5
-sum = i + j
-print (f' the sum is {sum}')
+for i in range(3):
+    for j in range(i):
+        print(f'{j}\t')
